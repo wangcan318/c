25702909cd893287fc1fce72c4b1deb6364a46a0
wangcan318 <65640544+wangcan318@users.noreply.github.com>
2022-07-21 12:33:29 +0800

    Add files via upload

---
+++ 数值运算函数.py
@@ -0,0 +1,8 @@
+print(abs(-30))#绝对值
+print(divmod(10,3))#商余
+print(pow(10,2))
+print(pow(5,2,7))#幂余 5的二次方25与7的余数
+print(round(2.54))
+print(round(3.1415926,2))#四舍五入保留2位
+print(max(1,3,6,8,10,0.5))#取最大值
+print(min(1,3,6,8,10,0.5))#取最小值
